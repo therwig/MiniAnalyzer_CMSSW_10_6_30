80cc6e68dad4d9a24c510a4f431a162304009d0b
Julie Hogan <jmhogan@fnal.gov>
2024-02-01 03:37:32 +0100

    add 2016 test settings

--- python/ConfFile_cfg.py
+++ python/ConfFile_cfg.py
@@ -4,12 +4,12 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:myfile.root'
+        'root://eospublic.cern.ch//eos/opendata/cms/Run2016G/SingleElectron/MINIAOD/UL2016_MiniAODv2-v2/120000/FF99404A-8F07-444E-B931-7B2AE327070B.root'
     )
 )
 
